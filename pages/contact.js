19de495af7d78eaddd882bba4f54df1d2f6fcb3c
Rodrigo <rodrigomp88@gmail.com>
2022-03-11 00:04:41 -0300

    contact prueba9

--- pages/contact.js
+++ pages/contact.js
@@ -10,15 +10,20 @@ const Contact = () => (
       </Heading>
       <SimpleGrid columns={[1, 1, 2]} gap={6}>
         <Section delay={0.1}>
-          <form method="POST" name="contact" data-netlify="true">
+          <form name="contact" method="POST" data-netlify="true">
             <p>
               <label>
-                Name <input type="text" name="name" />
+                Your Name: <input type="text" name="name" />
               </label>
             </p>
             <p>
               <label>
-                Email <input type="email" name="email" />
+                Your Email: <input type="email" name="email" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Message: <textarea name="message"></textarea>
               </label>
             </p>
             <p>
