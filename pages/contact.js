ce776f3800acdec0571042598edb697a29a5fa65
Rodrigo <rodrigomp88@gmail.com>
2022-03-11 00:51:26 -0300

    contact prueba 13

--- pages/contact.js
+++ pages/contact.js
@@ -13,21 +13,30 @@ const Contact = () => (
           <form name="contact" method="POST" data-netlify="true">
             <p>
               <label>
-                tu nombre: <input type="text" name="name" />
+                Your Name: <input type="text" name="name" />
               </label>
             </p>
             <p>
               <label>
-                Email: <input type="email" name="email" />
+                Your Email: <input type="email" name="email" />
               </label>
             </p>
             <p>
               <label>
-                Mensaje: <textarea name="message"></textarea>
+                Your Role:{' '}
+                <select name="role[]" multiple>
+                  <option value="leader">Leader</option>
+                  <option value="follower">Follower</option>
+                </select>
               </label>
             </p>
             <p>
-              <button type="submit">Enviar</button>
+              <label>
+                Message: <textarea name="message"></textarea>
+              </label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
             </p>
           </form>
         </Section>
