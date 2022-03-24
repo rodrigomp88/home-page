9711730cf467ec3d4397fbc62451042a3885d380
rodrigomp88 <rodrigomp88@gmail.com>
2022-03-24 20:16:58 -0300

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import { Box } from '@chakra-ui/react'
 const Footer = () => {
   return (
     <Box align="center" opacity={0.4} fontSize="sm">
-Hecho con ♥ con Next.js | Chakra.ui
+      Hecho con ♥ con Next.js | Chakra.ui
       &copy; {new Date().getFullYear()} Pinea Rodrigo. All Rights Reserved.
     </Box>
   )
